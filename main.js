a1a82fefd7e877062697fe4a1b4542644ff3b33f
Harby97 <ingharby@gmail.com>
2022-08-30 10:33:18 -0500

    fusion del carrito de compras, teniendo en cuenta que los menus se deben cerrar al abrir el carrito de compras y viceversa

--- main.js
+++ main.js
@@ -2,16 +2,41 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCarIcon = document.querySelector('.navbar-shopping-cart');
+const asideDetailingCar = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleburgerMenu);
+shoppingCarIcon.addEventListener('click', toggleshoppingCarMenu);
 
 function toggleDesktopMenu(){
+    const isShoppingCarOpen = !asideDetailingCar.classList.contains('inactive');
+
     desktopMenu.classList.toggle('inactive')
+    
+    if(isShoppingCarOpen){
+        asideDetailingCar.classList.add('inactive')
+    }
 }
 
 function toggleburgerMenu(){
+    const isShoppingCarOpen = !asideDetailingCar.classList.contains('inactive');
+    
     mobileMenu.classList.toggle('inactive')
-    console.log('cambiando menu mobile');
+
+    if(isShoppingCarOpen){
+        asideDetailingCar.classList.add('inactive')
+    }
+}
+
+function toggleshoppingCarMenu(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    asideDetailingCar.classList.toggle('inactive')
+    if(isMobileMenuOpen || isDesktopMenuOpen){
+        desktopMenu.classList.add('inactive')
+        mobileMenu.classList.add('inactive')
+    }
 }
